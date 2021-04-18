d65b71c1528ed56c3fde356e23c4cc5927731526
Nikita Zerniukov <Nikita.Zerniukov@urfu.me>
2021-04-19 01:19:41 +0500

    Configure propertyProvider

--- src/VKHandler/MainFrame.ts
+++ src/VKHandler/MainFrame.ts
@@ -16,4 +16,8 @@ vk.updates.on("message_new", sessionManager.middleware);
 vk.updates.on("message_new", sceneManager.middleware);
 vk.updates.on("message_new", sceneManager.middlewareIntercept);
 
+vk.updates.on("message_new", (context, next) => {
+  return context.scene.enter('abit'); //todo: implement SceneProvider with abit:Array<Scene> @garcher @midnighter
+})
+
 vk.updates.start().then(r => console.log("Start success!!!"));
