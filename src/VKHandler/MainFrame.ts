a4ee50140e32fee5398a9c2743815ca4d5678d9e
Nikita Zerniukov <Nikita.Zerniukov@urfu.me>
2021-04-18 22:55:54 +0500

    Configure propertyProvider

--- src/VKHandler/MainFrame.ts
+++ src/VKHandler/MainFrame.ts
@@ -1,3 +1,19 @@
 import {VK} from 'vk-io';
+import {SessionManager} from '@vk-io/session';
+import {SceneManager} from '@vk-io/scenes';
+let PropertyProvider = require('./PropertyProvider')
+import * as fs from 'fs';
 
-console.log('Scripts work correctly');
+
+const vk: VK = new VK({
+  token: new PropertyProvider().token
+});
+
+const sessionManager: SessionManager = new SessionManager<{}>();
+const sceneManager: SceneManager = new SceneManager();
+
+vk.updates.on("message_new", sessionManager.middleware);
+vk.updates.on("message_new", sceneManager.middleware);
+vk.updates.on("message_new", sceneManager.middlewareIntercept);
+
+vk.updates.start().then(r => console.log("Start success!!!"));
