f3b40821a6de66a87badd4a96d9026702bac102d
horkworse <44303647+horkworse@users.noreply.github.com>
2021-06-20 19:37:08 +0500

    Delete server/config directory

--- server/config/mongoOptions.ts
+++
@@ -1,9 +0,0 @@
-export class MongoOptions {
-
-  public static URI: string = 'mongodb+srv://develop:165423SirGay@store.axt36.mongodb.net/satellite?retryWrites=true&w=majority';
-
-  public static options = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  };
-}
